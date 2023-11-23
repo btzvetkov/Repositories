475d7c4e8e96c074e0bd445b2edfbc187a864d76
btzvetkov <boyan.tzvetkov@gmail.com>
2023-11-23 22:56:54 +0200

    new-day3

--- 100DaysofPython/Day 03/rollercoastercheck.py
+++ 100DaysofPython/Day 03/rollercoastercheck.py
@@ -1,11 +1,13 @@
 height = int(input("Please, enter your height in cm: "))
-
+bill = 0
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("Please, enter your age: "))
-    if age > 18:
+    if age > 18 and age < 45:
         bill = 12
         print("Adult tickets are $12")
+    elif age >= 45 and age <= 55:
+        print("You have a discount my friend, hope you feel better!")
     elif age >= 12 and age <= 18:
         bill = 7
         print("Youth tickets are $7")
